83bb8995283bf898ac394302c3deeb07c35943d3
Sean Biava <seanbiava@gmail.com>
2019-07-13 16:08:37 -0400

    npm run deploy has been run

--- src/components/social.js
+++ src/components/social.js
@@ -5,9 +5,9 @@ import '../css/global.css';
 const Social = () => {
     return (
         <div className='inlineWrapper social'>
-            <div className="inlineItem instagram"></div>
-            <div className="inlineItem facebook"></div>
-            <div className="inlineItem linkedin"></div>
+            <div className="inlineItem instagram hoveritem"></div>
+            <div className="inlineItem facebook hoveritem"></div>
+            <div className="inlineItem linkedin hoveritem"></div>
         </div>
       );
 }
